785cab3cf85f9e68107ace0b9a99be5e5fef1bf9
JunWoo0514 <ericcjy1990@gmail.com>
2020-08-10 10:02:39 +0800

    Report Update

--- MavenTest/src/test/java/AASTestCase/MavenTest/ProductModuleTest.java
+++ MavenTest/src/test/java/AASTestCase/MavenTest/ProductModuleTest.java
@@ -24,7 +24,7 @@ public class ProductModuleTest {
 	ExtraPTTest extraPTTest;
 	JavascriptExecutor js = (JavascriptExecutor) driver;
 	
-	static String testPT = "12.5";
+	static String testPT = "13.5";
 	
 	
 	@DataProvider(name="ExtraPTCase")
